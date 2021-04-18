0cd2627ea53087ac7761dd65e2a93e871a3055c0
Shannon Daly <shannon.theledge@gmail.com>
2021-04-18 16:21:59 +1000

    Add new timesheet entry page

--- app/controllers/timesheet_entries_controller.rb
+++ app/controllers/timesheet_entries_controller.rb
@@ -5,15 +5,24 @@ class TimesheetEntriesController < ApplicationController
   end
 
   def new
-
+    @timesheet_entry = TimesheetEntry.new
   end
 
   def create
-
+    @timesheet_entry = TimesheetEntry.new(timesheet_entry_params)
+    if @timesheet_entry.save
+      redirect_to timesheet_entries_path
+    end
   end
 
   def delete
 
+  end
+
+  private
+
+  def timesheet_entry_params
+    params.require(:timesheet_entry).permit(:date, :start_time, :finish_time)
   end
 
 end
